3fece87dc86ec2261793c3b1a23e4f326f58e6eb
AgapovaMariya <lps.mashka@gmail.com>
2025-09-29 02:55:38 +0700

    Add_image

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,3 +59,10 @@ fun DefaultPreview() {
     .padding(all = 10.dp) // Добавляет отступ в 10 пикселей со всех четырёх сторон
     .border(width = 2.dp, color = Color.Black) // Рисует чёрную рамку шириной 2 пикселя */
 
+@Composable
+fun CustomImage(image: Int) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = null
+    )
+}
